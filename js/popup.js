1dcb9bf5a91958ba4264ae54d2fb769dd19994b0
AnastasiaDor <doron.nastya@gmail.com>
2019-06-18 20:56:49 +0300

    Final

--- js/popup.js
+++ js/popup.js
@@ -1,40 +1,40 @@
+var popup=document.querySelector(".modal");
+var close=document.querySelector(".modal-close");
+var link = document.querySelector(".feedback");
+var login=popup.querySelector("[name=login]");
+var email=popup.querySelector("[name=email]");
+var form = document.querySelector(".user");
 
-  var popup=document.querySelector(".modal");
-  var close=document.querySelector(".modal-close");
-  var link = document.querySelector(".feedback");
-  var login=popup.querySelector("[name=login]");
-  var password=popup.querySelector("[name=password]");
-  var form = document.querySelector(".user");
+var isStorageSupport=true;
+var storage="";
 
-  var isStorageSupport=true;
-  var storage=localStorage.getItem("login");
+try {
+	storage=localStorage.getItem("login");
+} 
+catch (err) {
+	isStorageSupport=false;
+}
 
-  try{
-      storage=localStorage.getItem("login");
-  } catch (err){
-      isStorageSupport=false;
-  }
+link.addEventListener("click",function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
 
-  link.addEventListener("click",function (evt){
-      evt.preventDefault();
-      popup.classList.add("modal-show");
+	if (storage){
+		login.value=storage;
+		email.focus();
+	} else{
+		login.focus();
+	}
+});
 
-      if (storage){
-          login.value=storage;
-          password.focus();
-      } else{
-          login.focus();
-      }
-  });
+close.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
 
-  close.addEventListener("click", function(evt){
-      evt.preventDefault();
-      popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
-  });
-
-form.addEventListener("submit", function (evt){
-	if (!login.value|| !password.value){
+form.addEventListener("submit", function (evt) {
+	if (!login.value|| !email.value){
 		evt.preventDefault();
 		popup.classList.remove("modal-error");
 		popup.offsetWidth = popup.offsetWidth;
@@ -47,11 +47,11 @@ form.addEventListener("submit", function (evt){
 	}
 });
 
- window.addEventListener("keydown", function(evt){
-          if(evt.keyCode===27){
-              if (popup. classList.contains("modal-show")){
-                  evt.preventDefault();
-                  popup.classList.remove("modal-show");
-              }
-          }
+window.addEventListener("keydown", function(evt) {
+	if(evt.keyCode===27){
+		if (popup. classList.contains("modal-show")){
+			evt.preventDefault();
+			popup.classList.remove("modal-show");
+		}
+	}
 });
